59fde4737312657322e34c7f8d8dc383678f68fc
laurent GREARD <laurent.greard@gmail.com>
2026-04-18 17:31:00 +0200

    Bug 1

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import AuthGate from "./components/AuthGate";
 import Home from "./components/Home";
@@ -10,25 +10,23 @@ import EssPointSituationForm from "./components/forms/EssPointSituationForm";
 
 export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Header />
-        <AuthGate>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/bordereau" element={<BordereauForm />} />
-            <Route path="/emargement" element={<EmargementForm />} />
-            <Route path="/ess-feuille-presence" element={<EssFeuillePresenceForm />} />
-            <Route path="/ess-note-geva" element={<EssNoteGevaForm />} />
-            <Route path="/ess-point-situation" element={<EssPointSituationForm />} />
-          </Routes>
-        </AuthGate>
-        <footer className="mx-auto max-w-6xl px-4 py-8 text-xs text-gray-500">
-          <div className="border-t pt-4">
-            Besoin d'aide ? Contactez le support technique.
-          </div>
-        </footer>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <AuthGate>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bordereau" element={<BordereauForm />} />
+          <Route path="/emargement" element={<EmargementForm />} />
+          <Route path="/ess-feuille-presence" element={<EssFeuillePresenceForm />} />
+          <Route path="/ess-note-geva" element={<EssNoteGevaForm />} />
+          <Route path="/ess-point-situation" element={<EssPointSituationForm />} />
+        </Routes>
+      </AuthGate>
+      <footer className="mx-auto max-w-6xl px-4 py-8 text-xs text-gray-500">
+        <div className="border-t pt-4">
+          Besoin d'aide ? Contactez le support technique.
+        </div>
+      </footer>
+    </div>
   );
 }
